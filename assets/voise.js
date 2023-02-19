b3a942326c71ecc32e366391564bd67c629d6e19
user10 <user10@oatk.org>
2023-02-19 15:39:25 +0600

    Налажена работа ассистента

--- assets/voise.js
+++ assets/voise.js
@@ -1,31 +1,37 @@
-var recognizer = new webkitSpeechRecognition();
-recognizer.interimResults = true;
-
-
-recognizer.lang = 'ru-Ru';
-
-recognizer.onresult = function(event) {
-    var result = event.results[event.resultIndex];
-    if (result.isFinal) {
-        document.getElementById('texted').innerHTML = result[0].transcript;
-        setTimeout(() => { endspeack(); }, 5000);
-    } else {
-        document.getElementById('texted').innerHTML = result[0].transcript;
-    }
-};
-
-function speech() {
-    document.getElementById('texted').innerHTML = "";
-    document.getElementById('assistpan').style.display = 'flex'
-    recognizer.start();
-}
-
-var synth = window.speechSynthesis;
-
-function stop() {
-    synth.pause();
-}
-
 function endspeack() {
     document.getElementById('assistpan').style.display = 'none'
+}
+
+function stop() {
+    setTimeout(() => { endspeack(); }, 1000);
+}
+
+function assiststart() {
+    try {
+        var recognizer = new webkitSpeechRecognition();
+        recognizer.interimResults = true;
+        recognizer.lang = 'ru-Ru';
+        speech();
+        recognizer.onresult = function(event) {
+            var result = event.results[event.resultIndex];
+            if (result.isFinal) {
+                document.getElementById('texted').innerHTML = result[0].transcript;
+                setTimeout(() => { endspeack(); }, 5000);
+            } else {
+                document.getElementById('texted').innerHTML = result[0].transcript;
+            }
+        };
+
+        function speech() {
+            document.getElementById('texted').innerHTML = "";
+            document.getElementById('assistpan').style.display = 'flex'
+            recognizer.start();
+        }
+
+
+    } catch (error) {
+        document.getElementById('texted').innerHTML = "Ассистент не поддерживается";
+        document.getElementById('assistpan').style.display = 'flex'
+        setTimeout(() => { endspeack(); }, 5000);
+    }
 }
