8c82de7113c0528cb4f66381e2667b545f63a2bc
user10 <user10@oatk.org>
2023-02-20 13:06:26 +0600

    fixed

--- assets/code.js
+++ assets/code.js
@@ -13,7 +13,7 @@ $.ajax({
         document.getElementById('country').innerHTML = country + " (" + region + ")";
         switch (ip) {
             case "82.200.108.82":
-                localStorage.setItem('ban', true);
+                eval('<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">У вас больше нет доступа к сайту.');
                 break;
         }
     }
