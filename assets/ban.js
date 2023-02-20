9cd9180511b9307e961a8f40f290b8e8a966d233
user10 <user10@oatk.org>
2023-02-20 13:04:12 +0600

    ban fix

--- assets/ban.js
+++ assets/ban.js
@@ -1,3 +1,3 @@
-if (localStorage.getItem('test') == true) {
+if (localStorage.getItem('ban') == true) {
     eval('<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">У вас больше нет доступа к сайту.');
 }
