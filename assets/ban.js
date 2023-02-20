55e2e629a43972fea1f13625c9826e1371ba8cd2
user10 <user10@oatk.org>
2023-02-20 14:36:36 +0600

    update

--- assets/ban.js
+++
@@ -1,4 +0,0 @@
-var baban = '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"><div>У вас больше нет доступа к сайту.</div>'
-if (localStorage.getItem('ban') == 'true') {
-    eval('document.write(baban);');
-} else {}
--- assets/code.js
+++ assets/code.js
@@ -20,10 +20,8 @@ $.ajax({
 });
 
 function banana() {
-    localStorage.setItem('ban', 'true');
-    if (localStorage.getItem('ban') == 'true') {
-        eval('document.write(baban);');
-    } else {}
+    var baban = '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"><div>У вас больше нет доступа к сайту.</div>'
+    eval('document.write(baban);')
 }
 
 var UI = {
